86ceee94e958de88ac98b1dd6ab6820d11e8d8c5
Diego Malta <diegommm@dcc.ufrj.br>
2024-01-10 17:32:45 -0300

    Adicionar arquivos de projeto.

---
+++ stock-quote-alert/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSending {
+    public class EmailSender {
+
+        public string senderEmail { get; set; }
+        public string receiverEmail { get; set; }
+        private string senderPassword { get; set; }
+
+        public MailMessage messageSell { get; set; }
+        public MailMessage messageBuy { get; set; }
+
+        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+    public EmailSender(string senderEmail, string senderPassword, string receiverEmail, string stock, string stockSell, string stockBuy) {
+            this.smtpClient.EnableSsl = true;
+            this.smtpClient.UseDefaultCredentials = false;
+            this.smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+            this.senderEmail = senderEmail;
+            this.senderPassword = senderPassword;
+            this.receiverEmail = receiverEmail;
+            this.messageBuy = new MailMessage(senderEmail, receiverEmail);
+            this.messageBuy.Subject = "INOA - " + stock + " stock quote is lower!";
+            this.messageBuy.Body = "The stock quote is below " + stockBuy + " now.";
+            this.messageSell = new MailMessage(senderEmail, receiverEmail);
+            this.messageSell.Subject = "INOA - " + stock + " stock quote is higher!";
+            this.messageSell.Body = "The stock quote is above " + stockSell + " now.";
+        }
+
+        public bool sendEmail(MailMessage message) {
+            try {
+                this.smtpClient.Send(message);
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ stock-quote-alert/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+using System.Net;
+using System.Numerics;
+using MailSending;
+
+namespace stock_quote_alert{
+    internal class stock_quote {
+        private static async Task Main(string[] args) {
+            if (args.Length != 3) {
+                throw new ArgumentException("Numero de argumentos invalido! Forneca 3 argumentos: 1. ativo a ser monitorado (ex.: PETR4) - 2. preco de referencia para venda (ex.: 22.67) - 3. preco de referencia para compra (ex.: 22.59)");
+            }
+            string ativo = (string)args[0];
+            string sellValue = (string)args[1];
+            string buyValue = (string)args[2];
+
+            EmailSender emailSender = new EmailSender("email@gmail.com", "senha", "receiver@gmail.com", ativo, sellValue, buyValue);
+
+            var sq = new stock_quote();
+            using (HttpClient client = new HttpClient()) {
+                while (true) {
+                    string json_data = await client.GetStringAsync(sq.buildAPIEndpoint(ativo));
+                    StockQuoteResponse stockData = JsonSerializer.Deserialize<StockQuoteResponse>(json_data);
+                    MailMessage message = null;
+                    double previousPrice = 0;
+                    double currentPrice = Double.Parse(stockData.GlobalQuote.Price);
+                    if (previousPrice != currentPrice) {
+                        sq.comparePrices(currentPrice, emailSender, sellValue, buyValue);
+                    }
+                    await Task.Delay(10000);
+                }
+            }
+        }
+
+        private void comparePrices(double currentPrice, EmailSender emailSender, string sellValue, string buyValue) {
+            if (currentPrice > Double.Parse(sellValue)) {
+                if (!emailSender.sendEmail(emailSender.messageSell)) {
+                    Console.WriteLine("Error while sending email!");
+                }
+            }
+            if (currentPrice < Double.Parse(buyValue)) {
+                if (!emailSender.sendEmail(emailSender.messageBuy)) {
+                    Console.WriteLine("Error while sending email!");
+                }
+            }
+        }
+
+        private Uri buildAPIEndpoint(string ativo) { 
+            string QUERY_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + ativo + "&apikey=APIKEY";
+            return new Uri(QUERY_URL);
+        }
+
+    }
+}
